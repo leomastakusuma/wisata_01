7e8c613bdea4bf5c784d2d4dc7b2c03a226f8657
leomastakusuma@gmail.com <leomastakusuma@gmail.com>
2014-08-23 14:55:01 +0700

    a

--- application/controller/admin.php
+++ application/controller/admin.php
@@ -15,16 +15,23 @@ require 'application/templates/admin/view.php';
 class admin extends Controller {
     //put your code here
     public function index(){
-        require 'application/templates/admin/index.html';
+//        require 'application/templates/admin/index.html';
+        require 'application/templates/admin/header.html';
+        require 'application/templates/admin/contents.html';
+        
     }
-    public function test(){
-        $title  = 'Test';
-        $body   = '<p>Abc</p>';
-        $foter  = 'Leo';
-        views($title, $body, $foter);
-                
+    public function all(){
+        require 'application/templates/admin/header.html';
+        require 'application/templates/admin/contentsfull.html';         
     }
     public function table(){
         require 'application/templates/admin/tables.html';
     }
+    public function reservasi(){
+        require 'application/templates/admin/header.html';
+        require 'application/views/admin/reservasi/index.html';
+        require 'application/templates/admin/footer.html';
+    }
+    
+    
 }
